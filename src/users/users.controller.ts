387b04ee131555e311722f77b42d3c2c645866ec
Dan Gorea <dangorea9@gmail.com>
2023-08-18 17:17:05 +0300

    Added authentification and guard with custom decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,28 +3,64 @@ import {
 	Controller,
 	Delete,
 	Get,
+	NotFoundException,
 	Param,
 	Patch,
 	Post,
-	Query
+	Query,
+	Session,
+	UseGuards
 } from '@nestjs/common';
 import { CreateUserDto } from "./dtos/create-user.dto";
 import { UpdateUserDto } from "./dtos/ update-user.dto";
 import { UsersService } from "./users.service";
+import { Serialize } from "../interceptors/serialize.interceptor";
+import { UserDto } from "./dtos/user.dto";
+import { AuthService } from "./auth.service";
+import { CurrentUser } from "./decorator/current-user.decorator";
+import { User } from "./user.entity";
+import { AuthGuard } from "./guards/auth.guard";
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
-	constructor(private usersService: UsersService) {
+	constructor(private usersService: UsersService, private authService: AuthService) {
 	}
-
+	
+	@Get('/whoami')
+	@UseGuards(AuthGuard)
+	whoAmI(@CurrentUser() user: User) {
+		return user;
+	}
+	
+	@Post('/signout')
+	signOut(@Session() session: any) {
+		session.userId = null;
+	}
+	
 	@Post('/signup')
-	createUser(@Body() body: CreateUserDto) {
-		return this.usersService.create(body.email, body.password)
+	async createUser(@Body() body: CreateUserDto, @Session() session: any) {
+		const user = await this.authService.signup(body.email, body.password);
+		
+		session.userId = user.id;
+		return user;
+	}
+	
+	@Post('/signin')
+	async signIn(@Body() body: CreateUserDto, @Session() session: any) {
+		const user = await this.authService.signin(body.email, body.password);
+		
+		session.userId = user.id;
+		return user;
 	}
 
 	@Get(':id')
-	findUser(@Param('id') id: string) {
-		return this.usersService.findOne(parseInt(id));
+	async findUser(@Param('id') id: string) {
+		const user = await this.usersService.findOne(parseInt(id));
+		if (!user) {
+			throw new NotFoundException('User not found')
+		}
+		return user;
 	};
 
 	@Get()
